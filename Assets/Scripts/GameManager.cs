56e6fe8c6da1b8057997f7391beee73fd459fe64
Teofebano <teofebano.stei@gmail.com>
2015-03-25 12:04:11 +0700

    Maze Generator 1
    Udah bisa nge generate Mazenya, belum ber tekstur

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public Maze mazePrefab;
+	private Maze mazeInstance;
+
+
+	// Use this for initialization
+	void Start () {
+		BeginGame ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			RestartGame();
+		}
+	}
+
+	// Begin Game
+	void BeginGame(){
+		mazeInstance = Instantiate (mazePrefab) as Maze;
+		StartCoroutine(mazeInstance.Generate ());
+	}
+
+	// Restart Game
+	void RestartGame(){
+		StopAllCoroutines();
+		Destroy (mazeInstance.gameObject);
+		BeginGame ();
+	}
+}
